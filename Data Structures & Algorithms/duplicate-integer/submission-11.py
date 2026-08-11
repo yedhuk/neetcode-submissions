35946fa7490e356bafcdbbbd9452cab20ccdfac9
YedhuKrishna <yedhukrishnaykp@gmail.com>
2026-08-11 12:48:19 +0530

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums_set = {} # More of a hash map approach same can done by a hash table approach as well using set
+        for item in nums:
+            if item in nums_set:
+                return True
+            else:
+                nums_set[item] = 1
+        
+        return False
+        
